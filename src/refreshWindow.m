c46e2b5ce443bcb843ebd8c49755619d255cb0ca
Amit Zohar <amitzohar4@gmail.com>
2021-12-12 10:55:42 +0200

    organized

--- src/refreshWindow.m
+++
@@ -1,9 +0,0 @@
-function  refreshWindow(matrix,window)
-    for ii= 1:9
-        for jj= 1:9
-            if(~isnan(matrix(ii,jj)))
-                set(window.cell(ii,jj) , 'string',matrix(ii,jj));
-            end
-        end
-    end
-end
