deb5dbc6f63227a42c65bb5a4ade41c7870ac0a9
Amit Zohar <amitzohar4@gmail.com>
2021-12-12 10:54:43 +0200

    organized

--- Clear.m
+++
@@ -1,9 +0,0 @@
-function Clear(~, ~ , window)
-%this function clears all the cells, restarts the board
-
-    for ii= 1:9
-        for jj= 1:9
-             set(window.cell(ii,jj) , 'string',[], 'enable','on');
-        end
-    end
-end
